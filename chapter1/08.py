9c9cd936b72fce118a906a3bb24a7df4b1e964ec
horiso0921 <horiso0921@gmail.com>
2020-04-28 13:42:49 +0900

    [fix]問題08:の問題仕様に変更

--- chapter1/08.py
+++ chapter1/08.py
@@ -1,6 +1,6 @@
 from string import ascii_lowercase 
 def cipher(target):
     return "".join(map(lambda literal:
-        str(219 - ord(literal)) if literal in ascii_lowercase else literal, target))
+        chr(219 - ord(literal)) if literal in ascii_lowercase else literal, target))
     
-print(cipher("My name is Sota Horiuchi"))
+print(cipher("My name is Sota Horiuch1"))
