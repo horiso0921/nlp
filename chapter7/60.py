b0b0d062525c0055a6493ade7eb40fd0c937dd65
horiso0921 <horiso0921@gmail.com>
2020-06-30 13:13:33 +0900

    [fix]

--- chapter7/60.py
+++ chapter7/60.py
@@ -1,26 +1,22 @@
 import json
 import leveldb
-from collections import defaultdict
-
+​
 FNAME = "artist.json"
 FNAME_DB = "sample_DB"
-
+​
 def _60():
-    d = defaultdict(int)
+    db = leveldb.LevelDB(FNAME_DB)
     with open(FNAME,"r",encoding="UTF-8") as target:
         for line in target:
             data_json = json.loads(line)
-
+​
             # 注意点
             # NAMEの重複とareaがないやつもある
             # 重複はID を付けて識別
             # areaがないものはgetを使うことで無を取得するようにした
-            for t in data_json.get("tags",[]):
-                d[t["value"]] += 1
-    
-    l = sorted(d.items(), key=lambda x: -x[1])
-    print(l[:30])
-
-
+            key = "{}(id={})".format(data_json["name"], str(data_json["id"]))
+            value = data_json.get("area", " ")
+            db.Put(key.encode(), value.encode())
+​
 if __name__ == "__main__":
     _60()
